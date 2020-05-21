9e8e3d64d83e37f165bdfe8ba3521d2f71c5c969
Carlos Esquivel <esquivel101@gmail.com>
2020-05-20 21:56:23 -0600

    CB-4 Arreglando errores en los tests de app.

--- cool-baby/src/app/app.component.spec.ts
+++ cool-baby/src/app/app.component.spec.ts
@@ -1,12 +1,20 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import {TestBed, async} from '@angular/core/testing';
+import {AppComponent} from './app.component';
+
+import {AngularFireModule} from '@angular/fire';
+import {AngularFireAuthModule} from '@angular/fire/auth';
+import {environment} from '../environments/environment';
+import {AngularFireDatabaseModule} from '@angular/fire/database';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
+      imports: [
+        AngularFireModule.initializeApp(environment.firebaseConfig),
+        AngularFireAuthModule,
+        AngularFireDatabaseModule
       ],
+      declarations: [AppComponent]
     }).compileComponents();
   }));
 
