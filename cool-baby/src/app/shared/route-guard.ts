8ca2df3edabdfe3edc47aa2963c38abd12afedf3
Carlos Esquivel <esquivel101@gmail.com>
2020-05-31 17:36:46 -0600

    CB-73 Agregando router guard

---
+++ cool-baby/src/app/shared/route-guard.ts
@@ -0,0 +1,26 @@
+import {Injectable} from '@angular/core';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router} from '@angular/router';
+import {Observable} from 'rxjs';
+import {UserService} from './user.service';
+import {AngularFireAuth} from '@angular/fire/auth';
+
+@Injectable()
+export class RouteGuard implements CanActivate {
+  constructor(private router: Router, private firebaseAuth: AngularFireAuth) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    return new Promise((resolve, reject) => {
+      this.firebaseAuth.onAuthStateChanged(user => {
+        if (user) {
+          resolve(true);
+        } else {
+          this.router.navigate(['/login']);
+          resolve(false);
+        }
+      });
+    });
+  }
+}
