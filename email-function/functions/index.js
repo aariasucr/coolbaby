ee236561d35de8d2ae2b8cea325e0fb3430efb4c
Carlos Esquivel <esquivel101@gmail.com>
2020-06-26 22:45:45 -0600

    CB-55 implementacion email limpiando

--- email-function/functions/index.js
+++ email-function/functions/index.js
@@ -1,50 +1,34 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const nodemailer = require("nodemailer");
-const cors = require("cors")({ origin: true });
 admin.initializeApp();
+//require("dotenv").config();
 
-/**
- * Here we're using Gmail to send
- */
-let transporter = nodemailer.createTransport({
-  service: "gmail",
-  port: 465,
-  secure: true,
-  auth: {
-    user: "cesquivelscursos@gmail.com",
-    pass: "unodos3!",
-  },
-});
+const SENDER_EMAIL = "cesquivelscursos@gmail.com";
+const SENDER_PASSWORD = "unodos3!";
 
-exports.sendMail = functions.https.onRequest((req, res) => {
-  cors(req, res, () => {
-    // getting dest email by query string
-    const dest = req.query.dest;
-    const usuario = req.query.us;
-    const articulo = req.query.art;
-    const mailOptions = {
-      from: "CoolBaby <yourgmailaccount@gmail.com>",
-      to: dest,
-      subject: "Compra", //
-      html:
-        `<p style="font-size: 16px;">Posible compra</p>
-                <br />
-                El usuario ` +
-        usuario +
-        ` quiere comprar el producto ` +
-        articulo +
-        `
-                <img src="https://images.prod.meredith.com/product/fc8754735c8a9b4aebb786278e7265a5/1538025388228/l/rick-and-morty-pickle-rick-sticker" />
-            `, // email content in HTML
-    };
-
-    // returning result
-    return transporter.sendMail(mailOptions, (erro, info) => {
-      if (erro) {
-        return res.send(erro.toString());
-      }
-      return res.send("Sended");
+exports.sendMail = functions.database
+  .ref("/tentatives/{userId}")
+  .onCreate((snap, ctx) => {
+    const data = snap.val();
+    console.log("data:", data);
+    let authData = nodemailer.createTransport({
+      service: "gmail",
+      port: 465,
+      secure: true,
+      auth: {
+        user: SENDER_EMAIL,
+        pass: SENDER_PASSWORD,
+      },
     });
+    authData
+      .sendMail({
+        from: SENDER_EMAIL,
+        to: data.email,
+        subject: "Your submission Info",
+        text: "probando",
+        html: "probando",
+      })
+      .then((res) => console.log("successfully sent that mail"))
+      .catch((err) => console.log("error:", err));
   });
-});
