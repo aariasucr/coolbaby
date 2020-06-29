497766b7a1b9a1676c9a88498d9595a852181ec0
Carlos Esquivel <esquivel101@gmail.com>
2020-06-28 23:14:49 -0600

    CB-82 Arreglando sonar cloud

--- cool-baby/src/app/compras/compras.component.spec.ts
+++ cool-baby/src/app/compras/compras.component.spec.ts
@@ -2,23 +2,23 @@ import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {ComprasComponent} from './compras.component';
 
+import {AngularFireAuth} from '@angular/fire/auth';
+import {AngularFireDatabase} from '@angular/fire/database';
+import {AngularFireStorage} from '@angular/fire/storage';
+import {RouteGuard} from '../shared/route-guard';
+import {ProductsComponent} from '../products/products.component';
 import {RouterTestingModule} from '@angular/router/testing';
-import {routes} from '../app-routing.module';
-import {FormsModule, NgForm} from '@angular/forms';
-import {ToastrModule} from 'ngx-toastr';
-import {HomeComponent} from '../home/home.component';
 import {NavegacionComponent} from '../navegacion/navegacion.component';
 import {SalesComponent} from '../sales/sales.component';
 import {ProductDetailComponent} from '../product-detail/product-detail.component';
 import {LoginComponent} from '../login/login.component';
 import {RegistroComponent} from '../registro/registro.component';
+import {routes} from '../app-routing.module';
+import {FormsModule} from '@angular/forms';
+import {ToastrModule} from 'ngx-toastr';
+import {HomeComponent} from '../home/home.component';
 import {CatalogoComponent} from '../catalogo/catalogo.component';
 import {FileUploaderComponent} from '../file-uploader/file-uploader.component';
-import {AngularFireAuthModule, AngularFireAuth} from '@angular/fire/auth';
-import {AngularFireDatabaseModule, AngularFireDatabase} from '@angular/fire/database';
-import {AngularFireStorageModule, AngularFireStorage} from '@angular/fire/storage';
-import {RouteGuard} from '../shared/route-guard';
-import {ProductsComponent} from '../products/products.component';
 
 import * as Mocks from '../shared/mocks';
 import {PerfilComponent} from '../perfil/perfil.component';
